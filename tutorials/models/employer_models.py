e9ac9222e61356b7a5e171234e8ef0e6b5f1fc74
Trong Vu <genericbushes@gmail.com>
2025-02-05 12:33:13 +0000

    Added a page to post new job listings

--- tutorials/models/employer_models.py
+++ tutorials/models/employer_models.py
@@ -33,3 +33,19 @@ class Employer(User):
 
     def __str__(self):
         return f"{self.company_name} ({self.username})"
+
+
+class Job(models.Model):
+    employer = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name='jobs'
+    )
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    requirements = models.TextField(blank=True)
+    salary = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- tutorials/views/employer_views.py
+++ tutorials/views/employer_views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import user_passes_test
 from tutorials.models.employer_models import Employer
+from tutorials.forms.employer_forms import JobForm
 
 def is_employer(user):
     return user.role == 'Employer'
@@ -19,3 +20,16 @@ def view_reports(request):
 
 def view_settings(request):
     return render(request, 'employer_settings.html')
+
+def create_job_listing(request):
+    if request.method == 'POST':
+        form = JobForm(request.POST)
+        if form.is_valid():
+            job = form.save(commit=False)
+            job.employer = request.user 
+            job.save()
+            return redirect('employer_job_listings')  
+    else:
+        form = JobForm()
+
+    return render(request, 'employer_create_job_listing.html', {'form': form})
