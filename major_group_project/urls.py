5234d736f5494a36d896a60c419750c9c66b9507
Trong Vu <genericbushes@gmail.com>
2025-02-11 15:26:20 +0000

    Allowed employers to create and view job listings

--- major_group_project/urls.py
+++ major_group_project/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from tutorials.views.views import log_in, home_page, log_out, sign_up  
 from tutorials.views.admin_views import admin_home_page, admin_job_listings, admin_settings
-from tutorials.views.employer_views import employer_home_page, view_listings, view_reports, view_settings, create_job_listing
+from tutorials.views.employer_views import employer_home_page, view_listings, view_reports, view_settings, create_job_listing, job_detail_view
 
 
 
@@ -36,6 +36,7 @@ urlpatterns = [
     path('employer_reports', view_reports, name='employer_reports'),
     path('employer_settings', view_settings, name='employer_settings'),
     path('employer_create_job_listings', create_job_listing, name='employer_create_job_listing'),
+    path('jobs/<int:pk>/', job_detail_view, name='employer_job_detail'),
 
     path('admin_job_listings/', admin_job_listings, name='admin_job_listings'),
     path('admin_settings/', admin_settings, name='admin_settings'),
--- tutorials/views/employer_views.py
+++ tutorials/views/employer_views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import user_passes_test
-from tutorials.models.employer_models import Employer
+from tutorials.models.employer_models import Employer, Job
 from tutorials.forms.forms import SignUpForm
 from tutorials.forms.employer_forms import JobForm
 
@@ -14,7 +14,8 @@ def employer_home_page(request):
     return render(request, 'employers_home_page.html', {'employers': employers})
 
 def view_listings(request):
-    return render(request, 'employer_job_listings.html')
+    jobs = Job.objects.all()  
+    return render(request, 'employer_job_listings.html', {'jobs': jobs})
 
 def view_reports(request):
     return render(request, 'employer_reports.html')
@@ -54,6 +55,7 @@ def employer_login(request):
     else:
         form = LogInForm()
     return render(request, 'log_in.html', {'form': form})
+
 def create_job_listing(request):
     if request.method == 'POST':
         form = JobForm(request.POST)
@@ -61,8 +63,14 @@ def create_job_listing(request):
             job = form.save(commit=False)
             job.employer = request.user 
             job.save()
-            return redirect('employer_create_job_listings')  
+            return redirect('employer_job_listings')  
     else:
         form = JobForm()
 
     return render(request, 'employer_create_job_listing.html', {'form': form})
+
+def job_detail_view(request, pk):
+    # Attempt to retrieve the Job with the given primary key
+    job = get_object_or_404(Job, pk=pk)
+    # Render the 'job_detail.html' template, passing in the retrieved job
+    return render(request, 'jobs/employer_job_detail.html', {'job': job})
