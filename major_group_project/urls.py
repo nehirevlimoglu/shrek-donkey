b6734a523886bfa128b380eefe0f41ab561b9209
Nehir Evlimoglu <evlimoglunehir@gmail.com>
2025-02-13 15:36:55 +0000

    fixed a merge error

--- major_group_project/urls.py
+++ major_group_project/urls.py
@@ -16,16 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-<<<<<<< HEAD
 from tutorials.views.views import log_in, home_page, log_out  # Import your views
 from tutorials.views.applicant_views import applicants_home_page, applicants_account, applicants_applied_jobs, applicants_favourites 
-=======
 from tutorials.views.views import log_in, home_page, log_out, sign_up  
 from tutorials.views.admin_views import admin_home_page, admin_job_listings, admin_settings
 from tutorials.views.employer_views import employer_home_page, view_listings, view_reports, view_settings, create_job_listing
 
 
->>>>>>> origin/employers
 
 urlpatterns =[
     path('admin/', admin.site.urls),
@@ -33,12 +30,10 @@ urlpatterns =[
     path('log_in/', log_in, name='log-in'),  
     path('homepage/', home_page, name='home-page'),
     path('logout/', log_out, name='log-out'),
-<<<<<<< HEAD
     path('applicants_home_page/', applicants_home_page, name='applicants-home-page'), #m
     path('applicants_favourites/', applicants_favourites, name='applicants-favourites'),
     path('applicants-applied-jobs/', applicants_applied_jobs, name='applicants-applied-jobs'),
     path("applicants_account/", applicants_account, name="applicants-account"),
-=======
     path('admin_home_page/', admin_home_page, name='admin_home_page'),
     path('admin_job_listings', admin_job_listings, name='admin_job_listings'),
 
@@ -51,6 +46,5 @@ urlpatterns =[
     path('admin_job_listings/', admin_job_listings, name='admin_job_listings'),
     path('admin_settings/', admin_settings, name='admin_settings'),
     path('sign_up', sign_up, name='sign-up'),
->>>>>>> origin/employers
 ]
 
