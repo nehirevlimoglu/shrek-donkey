68e4afbf9498af3f233cdc79609fc6141fca2fd7
Nehir Evlimoglu <evlimoglunehir@gmail.com>
2025-02-14 16:59:43 +0000

    added a change password, and profile page

--- tutorials/views/employer_views.py
+++ tutorials/views/employer_views.py
@@ -3,7 +3,9 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import user_passes_test
 from tutorials.models.employer_models import Employer, Job
 from tutorials.forms.forms import SignUpForm, LogInForm
-from tutorials.forms.employer_forms import JobForm
+from tutorials.forms.employer_forms import JobForm, CustomPasswordChangeForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import update_session_auth_hash
 
 def is_employer(user):
     return hasattr(user, 'role') and user.role == 'Employer'
@@ -13,14 +15,11 @@ def employer_home_page(request):
     employers = Employer.objects.all()
     return render(request, 'employers_home_page.html', {'employers': employers})
 
-def view_listings(request):
-    jobs = Job.objects.all()
-    return render(request, 'employer_job_listings.html', {'jobs': jobs})
-
 def view_reports(request):
     return render(request, 'employer_reports.html')
 
-def view_settings(request):
+@login_required
+def employer_settings(request):
     return render(request, 'employer_settings.html')
 
 def employer_sign_up(request):
@@ -38,6 +37,19 @@ def employer_sign_up(request):
         form = SignUpForm()
     return render(request, 'sign_up.html', {'form': form})
 
+def create_job_listings(request):
+    if request.method == 'POST':
+        form = JobForm(request.POST)
+        if form.is_valid():
+            job = form.save(commit=False)
+            job.employer = request.user 
+            job.save()
+            return redirect('employer_job_listings')  
+    else:
+        form = JobForm()
+
+    return render(request, 'employer_create_job_listing.html', {'form': form})
+
 def employer_login(request):
     if request.method == 'POST':
         form = LogInForm(request, data=request.POST)
@@ -52,18 +64,16 @@ def employer_login(request):
         form = LogInForm()
     return render(request, 'log_in.html', {'form': form})
 
-def create_job_listing(request):
+@login_required
+def change_password(request):
     if request.method == 'POST':
-        form = JobForm(request.POST)
+        form = CustomPasswordChangeForm(user=request.user, data=request.POST)
         if form.is_valid():
-            job = form.save(commit=False)
-            job.employer = request.user
-            job.save()
-            return redirect('employer_job_listings')
+            form.save()
+            update_session_auth_hash(request, form.user)  # Prevent logout after password change
+            return redirect('password_change_done')  # Redirect to a success page
     else:
-        form = JobForm()
-    return render(request, 'employer_create_job_listing.html', {'form': form})
+        form = CustomPasswordChangeForm(user=request.user)
+    
+    return render(request, 'change_password.html', {'form': form})
 
-def job_detail_view(request, pk):
-    job = get_object_or_404(Job, pk=pk)
-    return render(request, 'jobs/employer_job_detail.html', {'job': job})
