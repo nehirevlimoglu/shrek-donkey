de756075a34def674e7f25cc73d010b9f1dd2313
Mert Ayranci <mert.ayranci@kcl.ac.uk>
2025-02-17 18:35:30 +0000

    applicants notifications

--- major_group_project/urls.py
+++ major_group_project/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from tutorials.views.views import log_in, home_page, log_out  # Import your views
-from tutorials.views.applicant_views import applicants_home_page, applicants_account, applicants_applied_jobs, applicants_favourites 
+from tutorials.views.applicant_views import applicants_home_page, applicants_account, applicants_applied_jobs, applicants_favourites, applicants_notifications 
 from tutorials.views.views import log_in, home_page, log_out, sign_up  
 from tutorials.views.admin_views import admin_home_page, admin_job_listings, admin_settings
 from tutorials.views.employer_views import employer_home_page, view_listings, view_reports, view_settings, create_job_listing
@@ -30,10 +30,13 @@ urlpatterns =[
     path('log_in/', log_in, name='log-in'),  
     path('homepage/', home_page, name='home-page'),
     path('logout/', log_out, name='log-out'),
+
     path('applicants_home_page/', applicants_home_page, name='applicants-home-page'), #m
     path('applicants_favourites/', applicants_favourites, name='applicants-favourites'),
     path('applicants_applied_jobs/', applicants_applied_jobs, name='applicants-applied-jobs'),
     path("applicants_account/", applicants_account, name="applicants-account"),
+    path('applicants_notifications/', applicants_notifications, name='applicants-notifications'),
+
     path('admin_home_page/', admin_home_page, name='admin_home_page'),
     path('admin_job_listings', admin_job_listings, name='admin_job_listings'),
 
