fa5d3f80038a70705a67039b46487aee9bea2d23
Mert Ayranci <mert.ayranci@kcl.ac.uk>
2025-02-20 17:49:14 +0000

    base

--- tutorials/models/applicants_models.py
+++ tutorials/models/applicants_models.py
@@ -7,7 +7,7 @@ class Applicant(models.Model):
     cv = models.FileField(upload_to='uploads/cv/', blank=True, null=True)
     salary_preferences = models.CharField(max_length=100, blank=True)
     job_preferences = models.TextField(blank=True)
-    location_preference s = models.CharField(max_length=150, blank=True)
+    location_preferences = models.CharField(max_length=150, blank=True)
 
     def __str__(self):
         return f"{self.user.username} - {self.degree}"
