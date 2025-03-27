b8dc51ac86ea9381eb95a597d6d0f480ccd9029c
04jjchou <151763933+04jjchou@users.noreply.github.com>
2025-03-27 10:11:38 +0800

    Update settings.py for deployment

--- major_group_project/settings.py
+++ major_group_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-if(vnx@arrljzcbhxag+))$_5%5d^y1f64$jh@s#*q%onf%9tk
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['junjiezhou.pythonanywhere.com']
+ALLOWED_HOSTS = ['JunjieZhou1.pythonanywhere.com']
 AUTH_USER_MODEL = 'tutorials.User'
 
 
